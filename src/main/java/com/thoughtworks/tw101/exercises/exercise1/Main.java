33df0251d42c3d2f41519f71bf75e4a77714cb1d
Rawradin <edgargdev@gmail.com>
2017-02-01 13:54:44 -0800

    Excercise 1 complete. Add build directory to .gitignore

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -4,6 +4,19 @@ package com.thoughtworks.tw101.exercises.exercise1;
 
 public class Main {
     public static void main(String[] args) {
+        int sum = printOddsAndReturnSum(1,100);
 
+        System.out.println("Sum is of previous odd numbers is " + sum);
+    }
+
+    private static int printOddsAndReturnSum(int startInt, int endInt) {
+        int sum = 0;
+        for(int i = startInt; i <= endInt; i++){
+            if(i % 2 == 1) {
+                sum += i;
+                System.out.println(i);
+            }
+        }
+        return sum;
     }
 }
