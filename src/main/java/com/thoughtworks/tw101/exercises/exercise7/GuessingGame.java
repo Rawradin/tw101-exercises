659d4fec01f2df8ccd1a32157fc94ccbe80a861b
Rawradin <edgargdev@gmail.com>
2017-02-02 18:56:19 -0800

    Excercise 7 done.

---
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise7/GuessingGame.java
@@ -0,0 +1,31 @@
+package com.thoughtworks.tw101.exercises.exercise7;
+
+import java.util.Scanner;
+
+/**
+ * Created by edgargonzalez on 2/2/17.
+ */
+public class GuessingGame {
+    private final int targetNumber;
+
+    public GuessingGame(int start, int end) {
+        this.targetNumber = RandomNumber.between(start, end);
+        System.out.println("  | Guessing Game (" + this.targetNumber + ") |  \nEnter a number between " + start + " - " + end);
+    }
+
+
+    public Boolean guess(Scanner input) {
+        String inputString = input.nextLine();
+        int inputNumber = Integer.parseInt(inputString);
+
+        if (inputNumber < this.targetNumber) {
+            System.out.println("Your number is lower than target.");
+        } else if (inputNumber > this.targetNumber) {
+            System.out.println("Your number is higher than target.");
+        } else {
+            System.out.println("YES! You guess the number!");
+            return true;
+        }
+        return false;
+    }
+}
--- src/main/java/com/thoughtworks/tw101/exercises/exercise7/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise7/Main.java
@@ -4,9 +4,18 @@ package com.thoughtworks.tw101.exercises.exercise7;
 // right then tell them they win, otherwise tell them if they guessed too high or low. Keep asking the user to guess
 // until they get the right answer. Use classes to separate the different concerns of this program.
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        GuessingGame game = new GuessingGame(1,100);
+        Scanner input = new Scanner(System.in);
+        Boolean won = false;
+
+        while(!won) {
+            won = game.guess(input);
+        }
 
     }
 }
