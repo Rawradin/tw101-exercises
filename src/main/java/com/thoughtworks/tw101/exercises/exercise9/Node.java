fa69432ed19343ea4b9741ac663828025ce1a02c
Rawradin <edgargdev@gmail.com>
2017-02-03 17:34:48 -0800

    Exercise 9 complete.

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -1,18 +1,53 @@
 package com.thoughtworks.tw101.exercises.exercise9;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
     private String name;
+    private Node leftNode;
+    private Node rightNode;
+    private static ArrayList<String> orderedNames = new ArrayList<>();
 
     public Node(String name) {
         this.name = name;
     }
 
     public void add(String nameOfNewNode) {
+        int compareResult = name.compareToIgnoreCase(nameOfNewNode);
+        if (compareResult > 0) {
+            if (leftNode != null) {
+                leftNode.add(nameOfNewNode);
+            } else {
+                leftNode = new Node(nameOfNewNode);
+            }
+        } else if (compareResult < 0) {
+            if (rightNode != null) {
+                rightNode.add(nameOfNewNode);
+            } else {
+                rightNode = new Node(nameOfNewNode);
+            }
+        } else {
+            System.out.println("This is the same string (not case sensitive)");
+        }
     }
 
     public List<String> names() {
-        return null;
+        if (leftNode != null) {
+            leftNode.names();
+        }
+        orderedNames.add(name);
+        if (rightNode != null){
+            rightNode.names();
+        }
+        return orderedNames;
+//        System.out.println("Printing out tree");
+//        System.out.println("L       R");
+//        System.out.println(leftNode.getName() + " <  >" + rightNode.getName());
+//        return new ArrayList<String>();
     }
+//
+//    public String getName() {
+//        return name;
+//    }
 }
