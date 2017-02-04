13a6f3b3e649fe7bb40c35f59ae1e9205f54b450
Rawradin <edgargdev@gmail.com>
2017-02-03 21:39:22 -0800

    Prime Factorization exercise complete

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -6,14 +6,41 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
     public static void main(String[] args) {
-        List<Integer> primeFactors = generate(30);
+        List<Integer> primeFactors = generate(2668);
+        for (int prime : primeFactors) {
+            System.out.println(prime);
+        }
     }
 
     private static List<Integer> generate(int n) {
-        return null;
+        ArrayList<Integer> factors = new ArrayList<>();
+        ArrayList<Integer> primes = new ArrayList<>();
+
+        for (int i = 2; i <= n/2; i++) {
+            if(n % i == 0) {
+                factors.add(i);
+            }
+        }
+
+        for (int factor : factors) {
+            if(isPrime(factor)) {
+                primes.add(factor);
+            }
+        }
+        return primes;
+    }
+
+    private static boolean isPrime(int n) {
+        for(int i = 2; i <= n/2; i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
